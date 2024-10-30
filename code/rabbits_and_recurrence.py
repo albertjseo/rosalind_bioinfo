590ee28f6204cf61f7d3092be14140f882d8a04e
albertjseo <albertjseo@gmail.com>
2024-10-30 15:06:46 -0700

    completion of rabbits_and_recurrence.py

---
+++ code/rabbits_and_recurrence.py
@@ -0,0 +1,12 @@
+# sample data set provided 5 & 3
+# start the Fibonacci sequence at 1
+
+def rabbit_recurrence(n, k):
+    parent, child = 1, 1
+    for months in range(1, n - 1):
+        child, parent = parent, parent + (child * k)
+    return parent
+    # test print
+
+
+print(rabbit_recurrence(5, 3))
